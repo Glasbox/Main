206297a76b18176ee6c3a47480c306428c89d49a
chra-odoo <chra@odoo.com>
2022-08-24 10:52:41 -0700

    [ADD] adding cloc_exclude to quickbooks connector manifest

--- pragmatic_quickbooks_connector_canada/__manifest__.py
+++ pragmatic_quickbooks_connector_canada/__manifest__.py
@@ -1,15 +1,25 @@
 {
-    'name': 'QuickBooks canada Online Odoo Connector',
-    'version': '1.5',
-    'category': 'Accounting',
-    'author': 'Pragmatic TechSoft Pvt Ltd.',
-    'website': 'http://www.pragtech.co.in',
-    'depends': ['hr', 'sale_management', 'purchase', 'account', 'stock', 'sale_purchase', 'sale_stock', 'stock_sms', 'account_check_printing'],
-    'external_dependencies': {
-        'python': ['xmltodict', 'requests'],
+    "name": "QuickBooks canada Online Odoo Connector",
+    "version": "1.5",
+    "category": "Accounting",
+    "author": "Pragmatic TechSoft Pvt Ltd.",
+    "website": "http://www.pragtech.co.in",
+    "depends": [
+        "hr",
+        "sale_management",
+        "purchase",
+        "account",
+        "stock",
+        "sale_purchase",
+        "sale_stock",
+        "stock_sms",
+        "account_check_printing",
+    ],
+    "external_dependencies": {
+        "python": ["xmltodict", "requests"],
     },
-    'summary': 'Odoo QuickBooks Bundle Odoo Quickbooks Desktop Connector Odoo Quickbooks integration QuickBooks Credit Memo Quickbooks reports odoo quickbooks connect accounting app accounting reports QuickBook Online connector online odoo accounting app QuickBook canada QuickBooks canada',
-    'description': """
+    "summary": "Odoo QuickBooks Bundle Odoo Quickbooks Desktop Connector Odoo Quickbooks integration QuickBooks Credit Memo Quickbooks reports odoo quickbooks connect accounting app accounting reports QuickBook Online connector online odoo accounting app QuickBook canada QuickBooks canada",
+    "description": """
 QuickBook Connector
 ====================
 Odoo Quickbooks online connector is used to export invoices/bill from Odoo get them paid in QBO and import paid invoices/bills in Odoo.
@@ -39,35 +49,41 @@ quickbooks online connector
 quickbooks online odoo 
 accounting app
 """,
-    'data': [
-        'data/qbo_data.xml',
-        'security/ir.model.access.csv',
-        'views/res_company_views.xml',
-        'views/export_partner.xml',
-        'views/account_views.xml',
-        'views/product_views.xml',
-        'views/res_partner_views.xml',
-        'views/details.xml',
-        'views/export_sale_order.xml',
-        'views/export_purchase_order.xml',
-        'views/export_dept.xml',
-        'views/export_emp.xml',
-        'views/res_config_settings.xml',
-        'views/refresh_token_cron.xml',
-        'views/import_journal_entry_cron.xml',
-        'wizard/connection_successfull_view.xml',
+    "data": [
+        "data/qbo_data.xml",
+        "security/ir.model.access.csv",
+        "views/res_company_views.xml",
+        "views/export_partner.xml",
+        "views/account_views.xml",
+        "views/product_views.xml",
+        "views/res_partner_views.xml",
+        "views/details.xml",
+        "views/export_sale_order.xml",
+        "views/export_purchase_order.xml",
+        "views/export_dept.xml",
+        "views/export_emp.xml",
+        "views/res_config_settings.xml",
+        "views/refresh_token_cron.xml",
+        "views/import_journal_entry_cron.xml",
+        "wizard/connection_successfull_view.xml",
         # 'views/import_customer_cron.xml',
         # 'views/import_sales_order_cron.xml',
-        'views/import_cron.xml',
-        'views/export_cron.xml',
+        "views/import_cron.xml",
+        "views/export_cron.xml",
         # 'views/qbo_logger.xml',
         # 'views/automated_authentication.xml',
     ],
-    'images': ['static/description/quickbook-connector_canada.gif'],
-    'live_test_url': 'http://www.pragtech.co.in/company/proposal-form.html?id=103&name=quickbook-connector',
-    'currency': 'USD',
-    'license': 'OPL-1',
-    'price': 250,
-    'installable': True,
-    'auto_install': False,
+    "images": ["static/description/quickbook-connector_canada.gif"],
+    "live_test_url": "http://www.pragtech.co.in/company/proposal-form.html?id=103&name=quickbook-connector",
+    "currency": "USD",
+    "license": "OPL-1",
+    "price": 250,
+    "installable": True,
+    "auto_install": False,
+    "cloc_exclude": [
+        "lib/common.py",  # exclude a single file
+        "data/*.xml",  # exclude all XML files in a specific folder
+        "example/**/*",  # exclude all files in a folder hierarchy recursively
+        "**/*.scss",  # exclude all scss file from the module
+    ],
 }
