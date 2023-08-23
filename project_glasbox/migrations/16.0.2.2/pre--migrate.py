0325ef71a817eb5739749e9315b7c631a9d04f45
Rwasenge <marw@odoo.com>
2023-08-23 19:09:49 -0400

    [UPG] added back missing lines in upgrade script

---
+++ project_glasbox/migrations/16.0.2.2/pre--migrate.py
@@ -0,0 +1,20 @@
+def migrate(cr, version):
+    # customization has parent, child
+    # oob has child, parent.
+    # old query 
+    query = """
+    delete from project_depending_tasks where task_id=depending_task_id;
+    
+    delete from project_depending_tasks where id not in (select max(id) 
+										   from project_depending_tasks
+										  group by task_id, depending_task_id);
+    
+    INSERT INTO task_dependencies_rel(task_id, depends_on_id)
+    SELECT depending_task_id, task_id FROM project_depending_tasks;
+    
+    UPDATE project_task 
+    SET planned_date_begin = date_start, planned_date_end = date_end
+    WHERE date_end > date_start;
+    """
+    cr.execute(query)
+    
