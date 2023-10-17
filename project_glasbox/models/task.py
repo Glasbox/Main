907de6d965b00a4d9b88b86768b0cb1ea3645187
Rwasenge <marw@odoo.com>
2023-10-17 15:21:06 -0400

    [FIX] skipping end date compute function if multiple projects detected

--- project_glasbox/models/task.py
+++ project_glasbox/models/task.py
@@ -22,6 +22,16 @@ class Company(models.Model):
             tasks._compute_holiday_days()
         return res
 
+# class ProjectCopy(models.Model):
+#     _inherit = "project.project"
+
+#     copied = fields.Boolean(default=False, copy=False)
+    
+#     def copy(self, default=None):
+#         new_project = super(ProjectCopy, self).copy(default)
+#         new_project.copied = True
+#         return new_project
+
 
 class TaskDependency(models.Model):
     _inherit = "project.task"
@@ -120,6 +130,10 @@ class TaskDependency(models.Model):
             if 'completion_date' in vals and vals['completion_date']:
                 record._send_mail_template()
         return res
+    
+    # def copy(self, default=None):
+    #     self = self.with_context({'copied': True})
+    #     return super(TaskDependency, self).copy(default)
 
     def _l_start_date(self):
         """
@@ -406,21 +420,25 @@ class TaskDependency(models.Model):
         """
         Computes the end date of a task applying forward calculation.
         date_end = date_start + planned_duration + buffer_time + on_hold
+        DO NOTING IF CALLED DURING A DUPLICATION 
+        delete later: when being duplicated this func is called with all tasks on both og and new project
+        check for when len(self.display_project_id) is 2 containing both projects, lack of context workaround
         """
-        offset = self.get_usertz_offset()
-        for record in self.filtered(lambda task: task.date_start):
-            new_start = record.date_start.replace(hour=(7), minute=0, second=0) - (timedelta(hours=offset))
-            record.write({'date_start': new_start})  # always set to 7am (offset by -5)
-            duration = (record.planned_duration + record.on_hold + record.buffer_time) - 1
-            if duration == 0:
-                record.write({'date_end': new_start + timedelta(hours=9)})
-            else:
-                new_end = record.get_forward_next_date(record.date_start, duration).replace(hour=(16), minute=0, second=0) - (timedelta(hours=offset))
-                record.write({'date_end': new_end})
-            record.planned_date_begin = record.date_start
-            if record.check_delay is False:
-                record.planned_date_end = record.date_end
-                record.update_planned_dates()
+        if len(self.display_project_id) == 1:
+            offset = self.get_usertz_offset()
+            for record in self.filtered(lambda task: task.date_start):
+                new_start = record.date_start.replace(hour=(7), minute=0, second=0) - (timedelta(hours=offset))
+                record.write({'date_start': new_start})  # always set to 7am (offset by -5)
+                duration = (record.planned_duration + record.on_hold + record.buffer_time) - 1
+                if duration == 0:
+                    record.write({'date_end': new_start + timedelta(hours=9)})
+                else:
+                    new_end = record.get_forward_next_date(record.date_start, duration).replace(hour=(16), minute=0, second=0) - (timedelta(hours=offset))
+                    record.write({'date_end': new_end})
+                record.planned_date_begin = record.date_start
+                if record.check_delay is False:
+                    record.planned_date_end = record.date_end
+                    record.update_planned_dates()
 
     @api.depends('l_end_date', 'planned_duration', 'milestone', 'scheduling_mode')
     def _compute_l_start_date(self):
